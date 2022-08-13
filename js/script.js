013eb37b114e5aa0f2a4627db523fa3a0b0b53ad
Evgeny Loy <evgenyloy@gmail.com>
2022-08-13 21:13:55 +0300

    v2

--- js/script.js
+++ js/script.js
@@ -15,3 +15,33 @@ link.forEach(el => {
         nav.classList.remove('nav_mobile');
     })
 })
+
+let toggles = document.getElementsByClassName('accordion__head');
+let contentDiv = document.getElementsByClassName('accordion__content');
+let accordion = document.querySelectorAll('.accordion__contentBx')
+
+
+for (let i = 0; i < toggles.length; i++) {
+    toggles[i].addEventListener('click', () => {
+        if (parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight) {
+            contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
+            accordion[i].classList.remove('active');
+            accordion[i].classList.add('active');
+
+        }
+        else {
+            contentDiv[i].style.height = "0px";
+            accordion[i].classList.remove('active');
+            /* accordion[i].classList.add('active'); */
+        }
+        for (let j = 0; j < contentDiv.length; j++) {
+            if (j !== i) {
+                contentDiv[j].style.height = "0px";
+                accordion[j].classList.remove('active');
+                /* accordion[j].classList.add('active'); */
+
+            }
+        }
+
+    })
+}
